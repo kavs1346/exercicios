8dc5e1cbdd2a566e45da036e4040bfd1a94afcde
kavs1346 <130669072+kavs1346@users.noreply.github.com>
2023-09-29 09:03:04 -0300

    (̶◉͛‿◉̶)

---
+++ exercicio1.php
@@ -0,0 +1,25 @@
+<?php
+    $num1 = 5;
+    $num2 = 2;
+    
+if($num1 > $num2) {
+    echo "Verdadeiro";
+} else {
+    echo "Falso";
+}
+
+    $robson = 1;
+    $pedro = 2;
+
+if($robson != $pedro){
+    echo "Robson é diferente de Pedro";
+} else {
+    echo "Robson é igual a Pedro";
+}
+
+if(12 <= 11 ) {
+    echo "Doze não é menor ou igual a onze";
+} else {
+    echo "Doze é menor ou igual a onze";
+}
+?>
